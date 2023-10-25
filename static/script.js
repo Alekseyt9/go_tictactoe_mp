d6e02e13bb32629a0e9a42e08440bfee52274ced
Alekseyt9 <alekseyt9@gmail.com>
2023-10-25 22:06:56 +0300

    +

--- static/script.js
+++ static/script.js
@@ -1,4 +1,3 @@
-
 var ws = new WebSocket("ws://localhost:3000/ws");
 ws.onopen = function() {
     ws.send("Hello, server!")
@@ -12,49 +11,69 @@ ws.onclose = function() {
     console.log("Connection closed")
 }
 
-const canvas = document.getElementById('board');
-const ctx = canvas.getContext('2d')
+const svgElement = document.getElementById('board');
 const count = 10;
 const size = 300/count;
 let move = false;
 
-canvas.addEventListener('click', (e) => {
-    const rect = canvas.getBoundingClientRect();
+svgElement.addEventListener('click', (e) => {
+    const rect = svgElement.getBoundingClientRect();
     const px = e.clientX - rect.left;
     const py = e.clientY - rect.top;
 
     const x = Math.floor(px/size) * size + size/2;
     const y = Math.floor(py/size) * size + size/2;
 
-    ctx.font = '45px sans-serif';
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
+
     move = !move;
-    
-    ctx.fillStyle = move ? "#00ff00" : "#0000ff";
-    const text = move ? "x" : "o";
-    ctx.fillText(text, x, y);
+    //textElement.setAttribute('fill', move ? "#0F2D70" : "#B33B1D");
+    if (move) {
+        drawCross(x, y, size);
+    } else {
+        drawCircle(x, y, size);
+    }
+
+    svgElement.appendChild(textElement);
 });
 
+function drawCross(x, y, size) {
+    const halfSize = size / 3.5; // половина длины крестика
+
+    drawLine(x - halfSize, y - halfSize, x + halfSize, y + halfSize, "#B33B1D", '3');
+    drawLine(x + halfSize, y - halfSize, x - halfSize, y + halfSize, "#B33B1D", '3');
+}
+
+function drawCircle(x, y, size) {
+    const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+    circle.setAttribute('cx', x);
+    circle.setAttribute('cy', y);
+    circle.setAttribute('r', size / 3.5); // радиус кружка
+    circle.setAttribute('stroke', "#0F2D70");
+    circle.setAttribute('stroke-width', '3');
+    circle.setAttribute('fill', 'none');
+
+    svgElement.appendChild(circle);
+}
 
 drawBoard(count, size);
 
-function drawLine(x1, y1,x2, y2) {
-    ctx.beginPath();
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
-    ctx.stroke();
+function drawLine(x1, y1, x2, y2, color, sw = '1') {
+    const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+    line.setAttribute('x1', x1);
+    line.setAttribute('y1', y1);
+    line.setAttribute('x2', x2);
+    line.setAttribute('y2', y2);
+    line.setAttribute('stroke', color);
+    line.setAttribute('stroke-width', sw);
+
+    svgElement.appendChild(line);
 }
 
 function drawBoard(count, size){
-    ctx.strokeStyle = 'black';
-    ctx.lineWidth = 2;
-
-    for (let i = 0; i < count-1; i++)
-    {
-        for (let j = 0; j < count-1; j++){
-            drawLine((i+1)*size, 0, (i+1)*size, count*size);
-            drawLine(0, (j+1)*size, count*size, (j+1)*size);
+    for (let i = 0; i < count-1; i++) {
+        for (let j = 0; j < count-1; j++) {
+            drawLine((i+1)*size, 0, (i+1)*size, count*size, '#696969');
+            drawLine(0, (j+1)*size, count*size, (j+1)*size, '#696969');
         }
     }
 }
